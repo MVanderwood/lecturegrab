239a7f6a6e7e043019f104d8ee7da5587675fdfd
MVanderwood <m.b.vanderwood@gmail.com>
2016-01-18 19:57:04 -0600

    Set up SMS message endpoint of queue job.

--- app/jobs/queue_job.rb
+++ app/jobs/queue_job.rb
@@ -2,18 +2,20 @@ class QueueJob < ActiveJob::Base
   queue_as :default
 
   def self.send_lecture(option)
-
-
-
-
-    #UNGODLY QUERY ISSUE ZOMG#
-    UserLecture.new({user_id: option.user_id. })
+    user = User.find_by(id: option.user_id)
+    lectures = []
+    subject_lectures = Lecture.where(subject_id: option.subject_id)
+    subject_lectures.each do |subject_lecture|
+      unattended_lectures << subject_lecture if subject_lecture.user_lectures.exists?(user_id: option.user_id) == false
+    end 
+    lecture = lectures.sample
     if option.delivery_method == "Text"
+      UserLecture.create({lecture_id: lecture.id, user_id: option.user_id, completion_date: DateTime.current})
       client = Twilio::REST::Client.new ENV["TWILIO_API_SID"], ENV["TWILIO_API_KEY"]
       message = client.messages.create({
-        to: "+18167296823",
+        to: "+1#{user.phone_number}",
         from: "+18168670795",
-        body: "Successs!!"
+        body: "#{lecture.title.upcase}:\n#{lecture}"
         # media_url: 
       })
     elsif option.delivery_method == "Email"
